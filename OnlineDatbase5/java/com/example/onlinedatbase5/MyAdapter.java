3ee6a4d7d85d86e2f125ae556650c19b622668bb
Hemal Ramani <104306754+hemlramani@users.noreply.github.com>
2022-07-21 14:15:42 +0530

    Add files via upload

---
+++ OnlineDatbase5/java/com/example/onlinedatbase5/MyAdapter.java
@@ -0,0 +1,54 @@
+package com.example.onlinedatbase5;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class MyAdapter extends BaseAdapter {
+    Context context;
+    List<Model> list;
+
+    public MyAdapter(Context context, List<Model> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return i;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        LayoutInflater inflater = LayoutInflater.from(context);
+        view = inflater.inflate(R.layout.design,viewGroup,false);
+
+        TextView txt1 = view.findViewById(R.id.txt1);
+        ImageView img1 = view.findViewById(R.id.img1);
+
+        txt1.setText(list.get(i).name);
+
+        Picasso.get().load(list.get(i).image).into(img1);
+
+        return view;
+    }
+}
